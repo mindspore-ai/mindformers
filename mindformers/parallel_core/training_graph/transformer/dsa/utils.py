dd21f8bd8a64d8074a9842e80020894cdb8a1cd4
魏琢艺 <weizhuoyi1@huawei.com>
2026-04-30 10:59:30 +0800

    dsa memory optimize

--- mindformers/parallel_core/training_graph/transformer/dsa/utils.py
+++ mindformers/parallel_core/training_graph/transformer/dsa/utils.py
@@ -13,8 +13,12 @@
 # limitations under the License.
 # ============================================================================
 """DSA utils."""
+from mindspore import nn, ops
+from mindspore.ops import auto_generate as aclnn_ops
 import mindspore.ops.functional as F
 from mindspore import dtype as mstype
+
+from mindformers.parallel_core.training_graph.device_matrix import layout
 
 
 def adjust_tnd_input(offset_id, q, k, actual_seq_qlen, actual_seq_klen):
@@ -38,3 +42,46 @@ def adjust_bsnd_input(offset_id, q, x):
     b, _, n, d = x.shape
     x = F.strided_slice(x, (0, 0, 0, 0), (b, offset_q, n, d), (1, 1, 1, 1))
     return x
+
+
+class WeightAbsorb(nn.Cell):
+    """weight absorb for MHA -> MQA"""
+    def __init__(self):
+        super().__init__()
+        self.bmm = aclnn_ops.BatchMatMul()
+        self.bs_transpose = aclnn_ops.Transpose()
+        self.transpose = aclnn_ops.Transpose()
+        self.reshape = aclnn_ops.Reshape()
+        infer_dtype = lambda *args: args[0]
+        self.absorb = ops.Morph(
+            self._absorb_func,
+            self.absorb_infer_shape,
+            infer_dtype
+        ).add_prim_attr("self_define_shard", True)
+        self.shard()
+
+    def absorb_infer_shape(self, *args):
+        s, b, n, _ = args[0]
+        d2 = args[1][-1]
+        return s, b, n, d2
+
+    def _absorb_func(self, x, x_absorb):
+        s, b, n, d1 = x.shape
+        d2 = x_absorb.shape[-1]
+        x = self.reshape(x, ((s * b), n, d1))
+        x = self.transpose(x, (1, 0, 2))
+        out = self.bmm(x, x_absorb)
+        out = self.reshape(self.transpose(out, (1, 0, 2)), (s, b, n, d2))
+        return out
+
+    def construct(self, x, absorb_weight, for_attn=False):
+        if for_attn:
+            x = self.bs_transpose(x, (1, 0, 2, 3))
+        return self.absorb(x, absorb_weight)
+
+    def shard(self):
+        self.bs_transpose.shard((layout("dp", "cp", "tp", "None"),))
+        self.absorb.shard(
+            (layout("cp", "dp", "tp", "None"), layout("tp", "None", "None")),
+            (layout("cp", "dp", "tp", "None"),)
+        )
